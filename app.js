1f5d21ebbe8da2cbbe1c51eb785c02be769d1196
AsepWilayana <asepwilayana@gmail.com>
2022-07-18 15:40:36 +0700

    CRUD final

--- app.js
+++ app.js
@@ -1,57 +1,168 @@
-const http = require('http');
-const fs = require('fs');
-const express = require('express')
-var expressLayouts = require('express-ejs-layouts');
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
+var expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
-var morgan = require('morgan')
-const {loadContact} = require ('./kontek');
- 
-// create "middleware"
-app.use(morgan('dev'))
+var morgan = require("morgan");
+const {
+  loadContact,
+  save_context,
+  validateAdd,
+  validateUpdate,
+  Deletedata,
+  Updatedata,
+} = require("./kontek");
 
-app.set('view engine', 'ejs')
+const { urlencoded } = require("express");
+
+// create "middleware"
+app.use(morgan("dev"));
+
+app.set("view engine", "ejs");
 
 // untuk membuka/akses image
-app.use(express.static('assets/'))
+app.use(express.static("assets/"));
 // app.use('/img', express.static((__dirname, 'assets/img')))
 
 app.use(expressLayouts);
-app.set('layout','layout/layout')
+app.set("layout", "layout/layout");
 
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) =>{
-    const nama = "Asep Wilayana";
-    const title = "Web Server EJS";
+// halaman home
+app.get("/", (req, res) => {
+  const nama = "Asep Wilayana";
+  const title = "Web Server EJS";
 
-    const cont = loadContact();
+  const cont = loadContact();
 
-    res.render('index', {nama:nama, title:title, cont})
-})
-app.get('/contact', (req, res) =>{
-    const title = "Web Server EJS";
-    const cont = loadContact();
-    res.render('contact', { title:title, cont})
-})
-app.get('/contact/:id', (req, res) =>{
-    const title = "Web Server EJS";
-    const name = req.params.id;
-    const contacts = loadContact();
-    const contact = contacts.find(item => item.name == name);
-    res.render('detail', { title:title, contact})
-})
-app.get('/about', (req, res) =>{
-    const title = "Web Server EJS";
-    res.render('about',{ title:title})
+  res.render("index", { nama: nama, title: title, cont });
+});
 
-})
-app.get('/product/:id', (req, res) =>{
-    res.send('product id :' + req.params.id + '<br/>' + 'category id :' + req.query.category)
-})
-app.use('/', (req,res) => {
-    res.status(404).send('page not found : 404');
-})
+// halaman contact
+// get semua contact
+app.get("/contact", (req, res) => {
+  const title = "Web Server EJS";
+  const cont = loadContact();
+  let respone = "";
+  if (req.query.deleted) respone = "Delete data berhasil";
+  if (req.query.updated) respone = "Updated data berhasil";
+  res.render("contact", { title: title, cont, respone });
+});
 
-.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`)
-})
+// halaman add contact
+app.get("/contact/add", (req, res) => {
+  const title = "Web Server EJS";
+  errors = "";
+  Errorname = "";
+  Erroremail = "";
+  Errormobile = "";
+
+  res.render("addContact", {
+    title: title,
+    errors,
+    Errorname,
+    Erroremail,
+    Errormobile,
+  });
+});
+
+// fungsi tambah contact
+app.post("/contact", validateAdd, (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const mobile = req.body.mobile;
+  save_context(name, email, mobile);
+
+  const title = "Web Server EJS";
+  const cont = loadContact();
+  const respone = "Data berhasil ditambahkan";
+
+  res.render("contact", { title: title, cont, respone });
+});
+
+// fungsi delete contact
+app.get("/delete/:id", (req, res) => {
+  const name = req.params.id;
+  Deletedata(name);
+
+  const title = "Web Server EJS";
+  const cont = loadContact();
+  const respone = "data berhasil dihapus";
+
+  res.redirect("/contact?deleted=success");
+});
+
+//halaman update contact
+app.get("/update/:id", (req, res) => {
+  const title = "Web Server EJS";
+  const name = req.params.id;
+  const contacts = loadContact();
+  const contact = contacts.find((item) => item.name == name);
+
+  errors = "";
+  Errorname = "";
+  Erroremail = "";
+  Errormobile = "";
+
+  res.render("updateContact", {
+    title: title,
+    contact,
+    errors,
+    Errorname,
+    Erroremail,
+    Errormobile,
+  });
+});
+
+// fungsi update
+app.post("/:id", validateUpdate, (req, res) => {
+  const name = req.params.id;
+  const Newname = req.body.name;
+  const Newemail = req.body.email;
+  const Newmobile = req.body.mobile;
+
+  Updatedata(name, Newname, Newemail, Newmobile);
+  const title = "Web Server EJS";
+  const cont = loadContact();
+  const respone = "data berhasil diupdate";
+  // res.render("contact", { title: title, cont, respone });
+  res.redirect("/contact?updated=success");
+});
+
+// get contact by id (detail)
+app.get("/contact/:id", (req, res) => {
+  const title = "Web Server EJS";
+  const name = req.params.id;
+  const contacts = loadContact();
+  const contact = contacts.find((item) => item.name == name);
+  if (!contact) {
+    res.status(404).send("page not found : 404");
+  }
+  res.render("detail", { title: title, contact });
+});
+
+// halaman about
+app.get("/about", (req, res) => {
+  const title = "Web Server EJS";
+  res.render("about", { title: title });
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(
+    "product id :" +
+      req.params.id +
+      "<br/>" +
+      "category id :" +
+      req.query.category
+  );
+});
+app
+  .use("/", (req, res) => {
+    res.status(404).send("page not found : 404");
+  })
+
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
--- kontek.js
+++ kontek.js
@@ -1,10 +1,179 @@
-const fs = require ("fs")
+const fs = require("fs");
+var validator = require("validator");
+const express = require("express");
+var expressLayouts = require("express-ejs-layouts");
+const app = express();
 
 const loadContact = () => {
-    const file = fs.readFileSync('data/contacts.json','utf8');
+  const file = fs.readFileSync("data/contacts.json", "utf8");
 
-    const contacts = JSON.parse(file);
-    return contacts;
+  const contacts = JSON.parse(file);
+  return contacts;
+};
+
+function validateName(name) {
+  const contacts = loadContact();
+  let findData = contacts.findIndex(
+    (item) => item.name.toLowerCase() == name.toLowerCase()
+  );
+  //console.log(findData)
+  if (findData >= 0) {
+    const inputan = name;
+    const msg = "nama sudah ada, silahkan gunakan nama lain";
+    Errorname = {
+      inputan,
+      msg,
+    };
+    errors.push(Errorname);
+  }
 }
 
-module.exports = {loadContact}
+function validateEmail(email) {
+  validemail = validator.isEmail(email);
+  if (validemail === false) {
+    const inputan = email;
+    const msg = "pengisian email tidak sesuai dengan format";
+    Erroremail = {
+      inputan,
+      msg,
+    };
+    errors.push(Erroremail);
+  }
+}
+
+function validateMobile(mobile) {
+  validphone = validator.isMobilePhone(mobile, "id-ID");
+  if (validphone === false) {
+    const inputan = mobile;
+    const msg = "pengisian nomor hp tidak sesuai";
+    Errormobile = {
+      inputan,
+      msg,
+    };
+    errors.push(Errormobile);
+  }
+}
+
+const validateAdd = (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const mobile = req.body.mobile;
+  errors = [];
+  const title = "Web Server EJS";
+  const contacts = loadContact();
+
+  validateName(name);
+  validateEmail(email);
+  validateMobile(mobile);
+
+  const jumlah = errors.length;
+  const id = req.params.id;
+  const contact = contacts.find((item) => item.name == id);
+
+  console.log(errors);
+  //res.render("updateContact", { title: title, msg, inputan, contact });
+  if (jumlah > 0) {
+    res.render("addContact", {
+      title: title,
+      errors,
+      Errorname,
+      Erroremail,
+      Errormobile,
+    });
+    return;
+  }
+  next();
+};
+
+const validateUpdate = (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const mobile = req.body.mobile;
+
+  errors = [];
+  const title = "Web Server EJS";
+
+  const contacts = loadContact();
+  validateName(name);
+  validateEmail(email);
+  validateMobile(mobile);
+
+  const jumlah = errors.length;
+  const id = req.params.id;
+  const contact = contacts.find((item) => item.name == id);
+
+  if (jumlah > 0 && id !== name) {
+    res.render("updateContact", {
+      title: title,
+      errors,
+      Errorname,
+      Erroremail,
+      Errormobile,
+      contact,
+    });
+    return;
+  }
+  next();
+};
+
+const save_context = (name, email, mobile) => {
+  //data yg akan di masukan file contact
+  const contact = {
+    name,
+    email,
+    mobile,
+  };
+  const contacts = loadContact();
+
+  contacts.push(contact);
+
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+  //tambhakan ke file
+  //rl.close();
+};
+
+const Deletedata = (name) => {
+  const contacts = loadContact();
+  const findData = contacts.find((item) => item.name == name);
+
+  if (findData !== undefined) {
+    const deletedData = contacts.filter((item) => item.name !== name);
+
+    fs.writeFileSync("data/contacts.json", JSON.stringify(deletedData));
+  }
+};
+
+const Updatedata = (name, newname, newemail, newmobile) => {
+  const contacts = loadContact();
+  const findData = contacts.find((item) => item.name == name);
+  if (findData !== undefined) {
+    //dihapus dulu data yg sudah ketemmu
+    const updateData = contacts.filter((item) => item.name !== name);
+
+    // buat objek baru
+    const contact = {
+      name: newname || findData.name,
+      email: newemail || findData.email,
+      mobile: newmobile || findData.mobile,
+    };
+
+    updateData.push(contact);
+
+    fs.writeFileSync("data/contacts.json", JSON.stringify(updateData));
+    // console.log(updateData);
+    // console.log('ada');
+  } else {
+    console.log("data tidak ada");
+    return;
+  }
+};
+
+module.exports = {
+  loadContact,
+  save_context,
+  validateAdd,
+  validateUpdate,
+  Deletedata,
+  Updatedata,
+};
